19ba7dc9e9f6a8e1973545554dae67d0e5dc93a4
NattyHam <160451333+NattyHam@users.noreply.github.com>
2024-11-14 17:29:56 +0000

    Fixed the code

--- test_zoo.py
+++ test_zoo.py
@@ -10,5 +10,20 @@ class TestZoo(unittest.TestCase):
        
     # Add your additional test cases here.
 
+    def test_tier0_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Error")
+
+    def test_tier1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    
+    def test_tier2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_tier3_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_tier3_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
 if __name__ == '__main__':
     unittest.main()
